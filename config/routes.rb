9f1cc1fa8407272a7617418a9815eabe069c7883
rhamill1 <rhamill11@gmail.com>
2017-04-01 18:21:04 -0700

    adds navigation to static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  root to: 'static_pages#home'
+
   get '/', to: 'static_pages#home'
   get '/about', to: 'static_pages#about'
   get '/company', to: 'static_pages#company'
