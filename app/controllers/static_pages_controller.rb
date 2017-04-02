eb70dcf2f76fc96ed2b07dc3b493dbc4d7a8f64e
rhamill1 <rhamill11@gmail.com>
2017-04-01 18:01:24 -0700

    improves route names and organization of static pages

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,13 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def company
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
-  root to: 'welcome#show'
+  get '/', to: 'static_pages#home'
+  get '/about', to: 'static_pages#about'
+  get '/company', to: 'static_pages#company'
+  get '/contact', to: 'static_pages#contact'
 
-  resources :welcome
   resources :admins
   resources :namespaces
   resources :users
